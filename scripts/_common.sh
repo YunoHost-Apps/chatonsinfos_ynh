a58f3fbc3d05b1e24d1d3f68e902e7a715753143
ljf <ljf@grimaud.me>
2023-08-08 10:25:19 +0200

    [wip] Config panel logic and scripts

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,6 +7,10 @@
 #=================================================
 # PERSONAL HELPERS
 #=================================================
+rewrite_config_panel() {
+    export apps=$(yunohost app list | grep "id\:" | sed "s/    id: //g")
+    ynh_render_template /etc/yunohost/apps/chatonsinfo/config_panel.toml.j2 /etc/yunohost/apps/chatonsinfo/config_panel.toml
+}
 
 #=================================================
 # EXPERIMENTAL HELPERS
